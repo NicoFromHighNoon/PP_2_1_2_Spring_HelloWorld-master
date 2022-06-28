b83acf78eb848e7cf381209413fc50effa7bc077
Nico <36701992+NicoFromHighNoon@users.noreply.github.com>
2022-06-28 09:13:32 +0900

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        
+        System.out.println(bean.getMessage());
+        System.out.println(bean.equals(bean2));
+        System.out.println(cat1.equals(cat2));
+    }
+}
